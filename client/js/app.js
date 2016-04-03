3298c58bb256173b31e17f1c69d75105be8f8fd1
Megan Smith <megan@tokaypress.com>
2016-04-03 13:16:34 -0700

    First Commit

---
+++ client/js/app.js
@@ -0,0 +1,25 @@
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    // this.state = {
+      // images: imageData,
+      // currentImageIndex: 0
+    // };
+  }
+  
+  render() {
+    // const {
+    //   images,
+    //   currentImageIndex
+    // } = this.state;
+
+    return (
+      <div>
+        <h3>IDEA BOARD</h3>
+      </div>
+    )
+  }
+}
+
+window.App = App;
